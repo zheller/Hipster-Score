1a31a58c0881f4f39d99ed7e8a36bbfdf6c435da
Zachary Heller <zheller@gmail.com>
2012-01-14 12:38:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipsterscore::Application.config.secret_token = '72f2c8de9c88d8e06a3fa9606a56c4aa54c29d07efe5474732d318166e0d7e0fe8fbc82d27097516463a1c8bc6fe2c7f622067a648634d6504cc65398854e3ba'
