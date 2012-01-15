a4a992551cc13a609e1381d354b583851c743e90
Nathan Bashaw <nbashaw@gmail.com>
2012-01-15 14:09:16 -0800

    deleted unused controller actions, implimented user facebook music saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,25 @@ class User
     key :email, String
     key :uid, Integer
     key :nickname, String
-    key :access_token, String 
+    key :access_token, String
+    key :artists, Array
     
     attr_accessible :name, :email, :nickname, :uid, :access_token
+    
+    def save_music()
+      raw_music = HTTParty.get("https://graph.facebook.com/#{self.uid}/music?access_token=#{self.access_token}")
+      music = ActiveSupport::JSON.decode(raw_music.body)
+      # iterate over artists and save to db
+      music['data'].each do |artist|
+        self.add_to_set(:artists => artist['name'])
+      end
+    end
+    
     def self.create_with_omniauth(auth)
       @user = User.create!(:name => auth["info"]["name"], :email => auth["info"]['email'], :uid => auth['uid'], :access_token => auth["credentials"]["token"], :nickname => auth["info"]["nickname"])
       @user.save!
+      @user.save_music()
       @user
     end
+    
 end
