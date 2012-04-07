88557bef728e81ad812a55b68932b42164902195
Nathan Bashaw <nbashaw@gmail.com>
2012-04-07 16:29:20 -0700

    removed the thing that only made it setup omniuath locally

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,7 @@
 if Rails.env.production?
   
 else
-  if ENV['USER'] == 'zachary' or 'nathan'
-    Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], {:client_options => {:ssl => {:ca_file => "#{Rails.root}/config/ca-bundle.crt"}}, :display => 'popup', :scope => 'email,user_religion_politics,user_about_me,user_likes,user_interests,user_education_history,user_actions:music,offline_access,publish_stream'}
-    end
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], {:client_options => {:ssl => {:ca_file => "#{Rails.root}/config/ca-bundle.crt"}}, :display => 'popup', :scope => 'email,user_religion_politics,user_about_me,user_likes,user_interests,user_education_history,user_actions:music,offline_access,publish_stream'}
   end
 end
