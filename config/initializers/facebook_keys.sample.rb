06d43027ccd92c7109b77a71430f186cddeadf2f
Nathan Bashaw <nbashaw@gmail.com>
2012-04-07 16:20:36 -0700

    added sample for facebook keys

---
+++ config/initializers/facebook_keys.sample.rb
@@ -0,0 +1,6 @@
+# Get the app ID and app secret at https://developers.facebook.com/apps/309828702393921
+
+
+# Set facebook app id
+ENV['APP_ID'] = 'app id here'
+ENV['APP_SECRET'] = 'app secret here'
